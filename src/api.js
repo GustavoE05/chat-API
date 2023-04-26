226df8f606deb4f64dac8f24c84110707128a0f7
GustavoE05 <gustavo-erheinheimer@educar.rs.gov.br>
2023-04-26 08:38:59 -0300

    conexão com mongo

--- src/api.js
+++ src/api.js
@@ -6,7 +6,7 @@ app.use(express.json())
 const router = express.Router();
 app.use('/', router.get('/', (req, res, next)=>{
     res.status(200).send("<h1>API - CHAT</h1>")
-}))
+}));
  
 app.use('/', router.get("/sobre", (req, res, next) =>{
     res.status(200).send({
@@ -14,6 +14,12 @@ app.use('/', router.get("/sobre", (req, res, next) =>{
     "versão":"0.1.0",
     "autor":"Gustavo bacana"
 })
-}))
+}));
 
-module.exports=app;
+app.use("/salas",router.get("/salas", (req,res, next) =>{
+    const salaController = require("./controllers/salaController");
+    let resp=salaController.get();
+    res.status(200).send(resp);
+}));
+
+module.exports = app;
---
+++ src/controllers/salaController.js
@@ -0,0 +1,7 @@
+const salaModel = require('../models/salaModel');
+
+exports.get=async()=>{
+    return await salaModel.listarSalas()
+}
+
+  
